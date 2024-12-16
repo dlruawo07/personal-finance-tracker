e98d88593e5bae547384d9526b3929353dc7a8a7
dlruawo07 <dlruawo07@gmail.com>
2024-12-16 17:54:18 +0900

    feat: start app

---
+++ frontend/lib/screens/sign_up_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          '',
+          style: TextStyle(
+            fontSize: 40,
+          ),
+        ),
+      ),
+    );
+  }
+}
